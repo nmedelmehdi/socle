fd2e4b22c001379d5be389e44a3bd1701a927dcd
nmedelmehdi <your-email>
2015-08-15 23:18:30 +0100

    first commit

---
+++ application/__modules/info/models/User.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+use \Illuminate\Database\Eloquent\Model as Eloquent;
+ 
+class User extends Eloquent{
+	protected $table = 'user';
+    
+}
---
+++ application/cache/twig/f/f/ff0081c87c1d40ec2017337c2fa13f6dac031a9accdffec8d44cc6a5c56da72b.php
@@ -0,0 +1,36 @@
+<?php
+
+/* hello.twig */
+class __TwigTemplate_ff0081c87c1d40ec2017337c2fa13f6dac031a9accdffec8d44cc6a5c56da72b extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : null), "html", null, true);
+    }
+
+    public function getTemplateName()
+    {
+        return "hello.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
